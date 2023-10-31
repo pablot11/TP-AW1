312a964bad51fa49c0fcb4502770ca89cc69d2b5
pablot11 <pablo564903@gmail.com>
2023-10-31 17:39:33 -0300

    arreglos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
-const app = document.getElementById('header');
+import peliculas from "./peliculas.js"
+const app = document.getElementById('main');
+let renderizadoHtml = "";
+peliculas.peliculas.forEach(pelicula => {
+    renderizadoHtml = renderizadoHtml + `<figure class="main-movies__figure">
+                                                <img src="assets/img/${pelicula.imagen}" alt="img-pelicula">
+                                                <figcaption>${pelicula.nombre}</figcaption>
+                                          </figure>`
+});
+app.innerHTML = `
+    <h1 class="main__h1">Nuestro Catalogo de Peliculas</h1>
+    <div class="main__search">
+        <input class="main__input-search" type="search" placeholder="Búsqueda" name="search">
+        <button class="main__input-button" type="submit">Buscar</button>
+    </div>
+    <div class="main-movies container">
+        ${renderizadoHtml}
+    </div>
+
+
+`
---
+++ assets/js/peliculas.js
@@ -0,0 +1,37 @@
+const peliculas = {
+        nombre:"Peliculas de Acción",
+        peliculas:[
+            {
+            nombre: "Casino",
+            imagen: "casino.jpg",
+            año: 1995,
+            genero: "Accion"
+            },
+            {
+            nombre: "El Club de la Pelea",
+            imagen: "clubpelea.jpg",
+            año: 1995,
+            genero: "Accion"
+            },
+            {
+            nombre: "Tarde de Perros",
+            imagen: "tardeperros.jpg",
+            año: 1995,
+            genero: "Accion"
+            },
+            {
+            nombre: "El Padrino",
+            imagen: "elpadrino.jpg",
+            año: 1995,
+            genero: "Accion"
+            },
+            {
+                nombre: "El Padrino II",
+                imagen: "elpadrino2.jpg",
+                año: 1995,
+                genero: "Accion"
+                }  
+         
+        ]
+    }
+export default peliculas
