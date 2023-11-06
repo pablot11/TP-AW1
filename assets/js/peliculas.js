70c2b2452eb64a1ed91c0a500dd820172e446076
pablot11 <pablo564903@gmail.com>
2023-11-05 22:01:20 -0300

    hola

--- assets/js/peliculas.js
+++ assets/js/peliculas.js
@@ -1,37 +1,43 @@
-const peliculas = {
-        nombre:"Peliculas de Acción",
+const peliculas = [
+        {
+        genero:"accion",
         peliculas:[
             {
-            nombre: "Casino",
-            imagen: "casino.jpg",
-            año: 1995,
-            genero: "Accion"
+                nombre: "Casino",
+                imagen: "casino.jpg",
+                año: 1995
             },
             {
-            nombre: "El Club de la Pelea",
-            imagen: "clubpelea.jpg",
-            año: 1995,
-            genero: "Accion"
+                nombre: "El Club de la Pelea",
+                imagen: "clubpelea.jpg",
+                año: 1995
             },
             {
-            nombre: "Tarde de Perros",
-            imagen: "tardeperros.jpg",
-            año: 1995,
-            genero: "Accion"
+                nombre: "Tarde de Perros",
+                imagen: "tardeperros.jpg",
+                año: 1995
             },
             {
-            nombre: "El Padrino",
-            imagen: "elpadrino.jpg",
-            año: 1995,
-            genero: "Accion"
+                nombre: "El Padrino",
+                imagen: "elpadrino.jpg",
+                año: 1995
             },
             {
                 nombre: "El Padrino II",
                 imagen: "elpadrino2.jpg",
                 año: 1995,
-                genero: "Accion"
-            }  
-         
+            }           
+        ]
+    },
+    {
+        genero:"Romance",
+        peliculas:[
+            {
+                nombre: "¿Conoces a Joe Black?",
+                imagen: "joeblack.jpg",
+                año: 1998
+            }
         ]
     }
+]
 export default peliculas
