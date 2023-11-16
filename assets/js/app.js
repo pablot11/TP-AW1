ccd119376933da6158d0d7ef5dcbceee532f586a
pablot11 <pablo564903@gmail.com>
2023-11-16 17:55:00 -0300

    TP3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,21 @@
-import peliculas from "./peliculas.js"
-/*
-const app = document.getElementById('main');
-let renderizadoHtml = "";
-peliculas.peliculas.forEach(pelicula => {
-    renderizadoHtml = renderizadoHtml + `<figure class="main-movies__figure">
-                                                <img src="assets/img/${pelicula.imagen}" alt="img-pelicula">
-                                                <figcaption>${pelicula.nombre}</figcaption>
-                                          </figure>`
-});
-app.innerHTML = `
-    <h1 class="main__h1">Nuestro Catalogo de Peliculas</h1>
-    <div class="main__search">
-        <input class="main__input-search" type="search" placeholder="Búsqueda" name="search">
-        <button class="main__input-button" type="submit">Buscar</button>
-    </div>
-    <div class="main-movies container">
-        ${renderizadoHtml}
-    </div>
-
-
-`
-*/
-
+const app = document.getElementById("contenedor-peliculas");
+let html = "";
 fetch('/json/peliculas.json').then((dato)=>{
-    return dato
+    return dato.json()
 }).then((dato)=>{
-    console.log(dato);
+    let peliculas = dato.peliculas.pelicula
+    renderizarHtml(peliculas)
+    app.innerHTML=html;
 })
 
+function renderizarHtml(peliculas){
+    peliculas.forEach(pelicula => {
+        html = html + `<figure class="main-movies__figure">
+                            <img src="assets/img/${pelicula.imagen}" alt="img-pelicula">
+                            <figcaption> ${pelicula.nombre} (${pelicula.año})</figcaption>
+                        </figure>`
 
+    });
 
+}
 
